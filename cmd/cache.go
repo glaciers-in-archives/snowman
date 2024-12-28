glaciers-in-archives/snowman

commit d63c1b98942e85ac2cc4da7ce74b66a0e54e02df

Allow users to specify a custom .snowman directory #106
side effects:

 - refactor cache manager to be independent of the SPARQL client
 - refactor SPARQL client to take a cacheManager as an argument instead
   of creating its own

cmd/cache.go
@@ -14,6 +14,7 @@ import (
 
 var invalidateCacheOption bool
 var unusedOption bool
+var snowmanPath string
 
 func printFileContents(path string) error {
 	fmt.Println(path)
@@ -47,27 +48,34 @@ var cacheCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var selectedCacheItems []string
 
+		// we do initialize the cache manager, so that all cache related checks are done
+		cm, err := cache.NewCacheManager("available", snowmanPath) // the cache strategy isn't relevant for this command
+		if err != nil {
+			return utils.ErrorExit("Failed to create cache manager.", err)
+		}
+		var cacheLocation = cm.SnowmanDirectoryPath + "/cache/"
+
 		if len(args) == 0 && !unusedOption {
-			totFiles, err := utils.CountFilesRecursive(cache.CacheLocation)
+			totFiles, err := utils.CountFilesRecursive(cacheLocation)
 			if err != nil {
 				return utils.ErrorExit("Failed to retrive cache info.", err)
 			}
 
 			fmt.Println("There are " + fmt.Sprint(totFiles) + " cache items.")
 
-			selectedCacheItems = append(selectedCacheItems, cache.CacheLocation)
+			selectedCacheItems = append(selectedCacheItems, cacheLocation)
 		} else if len(args) == 0 && unusedOption {
 			usedItems, err := utils.ReadLineSeperatedFile(".snowman/last_build_queries.txt")
 			if err != nil {
 				return utils.ErrorExit("Failed to read last unused cache items: ", err)
 			}
 
-			err = fs.WalkDir(os.DirFS("."), cache.CacheLocation, func(path string, info fs.DirEntry, err error) error {
+			err = fs.WalkDir(os.DirFS("."), cacheLocation, func(path string, info fs.DirEntry, err error) error {
 				if err != nil {
 					return err
 				}
 
-				pathAsCacheItem := strings.Replace(strings.Replace(path, ".json", "", 1), cache.CacheLocation, "", 1)
+				pathAsCacheItem := strings.Replace(strings.Replace(path, ".json", "", 1), cacheLocation, "", 1)
 				isUsed := false
 				for _, used := range usedItems {
 					if pathAsCacheItem == used || strings.HasPrefix(used, pathAsCacheItem) {
@@ -83,7 +91,7 @@ var cacheCmd = &cobra.Command{
 
 			fmt.Println("Found " + fmt.Sprint(len(selectedCacheItems)) + " unused cache items.")
 		} else if len(args) == 1 {
-			dirPath := cache.CacheLocation + cache.Hash(args[0])
+			dirPath := cacheLocation + cache.Hash(args[0])
 
 			files, err := os.ReadDir(dirPath)
 			if err != nil {
@@ -106,7 +114,7 @@ var cacheCmd = &cobra.Command{
 
 			queryString := strings.Replace(string(sparqlBytes), "{{.}}", args[1], 1)
 
-			filePath := cache.CacheLocation + cache.Hash(args[0]) + "/" + cache.Hash(queryString) + ".json"
+			filePath := cacheLocation + cache.Hash(args[0]) + "/" + cache.Hash(queryString) + ".json"
 			selectedCacheItems = append(selectedCacheItems, filePath)
 
 			printFileContents((filePath))
@@ -129,4 +137,5 @@ func init() {
 	rootCmd.AddCommand(cacheCmd)
 	cacheCmd.Flags().BoolVarP(&invalidateCacheOption, "invalidate", "i", false, "Removes/clears the specified parts of the query cache.")
 	cacheCmd.Flags().BoolVarP(&unusedOption, "unused", "u", false, "Returns cache items not used in the last build.")
+	cacheCmd.Flags().StringVarP(&snowmanPath, "snowman-directory", "d", ".snowman", "Sets the snowman directory to use.")
 }

internal/cache/cache.go
@@ -11,8 +11,6 @@ import (
 	"github.com/glaciers-in-archives/snowman/internal/utils"
 )
 
-var CacheLocation string = ".snowman/cache/"
-
 func Hash(value string) string {
 	hash := sha256.Sum256([]byte(value))
 	return hex.EncodeToString(hash[:])
@@ -22,15 +20,17 @@ type CacheManager struct {
 	CacheStrategy          string // "available", "never"
 	StoredCacheHashes      map[string]bool
 	CacheHashesUsedInBuild []string
+	SnowmanDirectoryPath   string
 }
 
-func NewCacheManager(strategy string) (*CacheManager, error) {
+func NewCacheManager(strategy string, snowmanDirectoryPath string) (*CacheManager, error) {
 	cm := CacheManager{
-		CacheStrategy: strategy,
+		CacheStrategy:        strategy,
+		SnowmanDirectoryPath: snowmanDirectoryPath,
 	}
 	cm.StoredCacheHashes = make(map[string]bool)
 
-	if err := os.MkdirAll(CacheLocation, 0770); err != nil {
+	if err := os.MkdirAll(cm.SnowmanDirectoryPath + "/cache/", 0770); err != nil {
 		return nil, err
 	}
 
@@ -45,13 +45,13 @@ func NewCacheManager(strategy string) (*CacheManager, error) {
 
 func (cm *CacheManager) readStoredHashes() error {
 	// index cache hashes
-	locationHashes, err := ioutil.ReadDir(CacheLocation)
+	locationHashes, err := ioutil.ReadDir(cm.SnowmanDirectoryPath + "/cache/")
 	if err != nil {
 		return err
 	}
 
 	for _, locationDirInfo := range locationHashes {
-		contentDirInfo, err := ioutil.ReadDir(CacheLocation + locationDirInfo.Name())
+		contentDirInfo, err := ioutil.ReadDir(cm.SnowmanDirectoryPath + "/cache/" + locationDirInfo.Name())
 		if err != nil {
 			return err
 		}
@@ -72,7 +72,7 @@ func (cm *CacheManager) GetCache(location string, query string) (*os.File, error
 		return nil, nil
 	}
 
-	queryCacheLocation := CacheLocation + fullQueryHash + ".json"
+	queryCacheLocation := cm.SnowmanDirectoryPath + "/cache/" + fullQueryHash + ".json"
 
 	return os.Open(queryCacheLocation)
 }
@@ -83,7 +83,7 @@ func (cm *CacheManager) SetCache(location string, query string, content string) 
 	}
 
 	fullQueryHash := Hash(location) + "/" + Hash(query)
-	queryCacheLocation := CacheLocation + fullQueryHash + ".json"
+	queryCacheLocation := cm.SnowmanDirectoryPath + "/cache/" + fullQueryHash + ".json"
 
 	if err := os.MkdirAll(filepath.Dir(queryCacheLocation), 0770); err != nil {
 		return err
@@ -108,7 +108,7 @@ func (cm *CacheManager) SetCache(location string, query string, content string) 
 }
 
 func (cm *CacheManager) Teardown() error {
-	if err := utils.WriteLineSeperatedFile(cm.CacheHashesUsedInBuild, ".snowman/last_build_queries.txt"); err != nil {
+	if err := utils.WriteLineSeperatedFile(cm.CacheHashesUsedInBuild, cm.SnowmanDirectoryPath+"/last_build_queries.txt"); err != nil {
 		return err
 	}
 

cmd/build.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/glaciers-in-archives/snowman/internal/config"
+	"github.com/glaciers-in-archives/snowman/internal/cache"
 	"github.com/glaciers-in-archives/snowman/internal/sparql"
 	"github.com/glaciers-in-archives/snowman/internal/static"
 	"github.com/glaciers-in-archives/snowman/internal/utils"
@@ -21,6 +22,7 @@ import (
 var cacheBuildOption string
 var staticBuildOption bool
 var configFileLocation string
+var snowmanDirectoryPath string
 
 func DiscoverLayouts() ([]string, error) {
 	var paths []string
@@ -100,7 +102,12 @@ var buildCmd = &cobra.Command{
 			return utils.ErrorExit("Failed to index query files.", err)
 		}
 
-		err = sparql.NewRepository(cacheBuildOption, queries, verbose)
+		cacheManager, err := cache.NewCacheManager(cacheBuildOption, snowmanDirectoryPath)
+		if err != nil {
+			return utils.ErrorExit("Failed to initiate cache manager.", err)
+		}
+
+		err = sparql.NewRepository(*cacheManager, queries, verbose)
 		if err != nil {
 			return utils.ErrorExit("Failed to initiate SPARQL client.", err)
 		}
@@ -188,4 +195,5 @@ func init() {
 	buildCmd.Flags().StringVarP(&cacheBuildOption, "cache", "c", "available", "Sets the cache strategy. \"available\" will use cached SPARQL responses when available and fallback to making queries. \"never\" will ignore existing cache and will not update or set new cache.")
 	buildCmd.Flags().BoolVarP(&staticBuildOption, "static", "s", false, "When set Snowman will only build static files.")
 	buildCmd.Flags().StringVarP(&configFileLocation, "config", "f", "snowman.yaml", "Sets the config file to use.")
+	buildCmd.Flags().StringVarP(&snowmanDirectoryPath, "snowman-directory", "d", ".snowman", "Sets the snowman directory to use.")
 }

internal/sparql/sparql.go
@@ -22,13 +22,13 @@ type Repository struct {
 	client       config.ClientConfig
 	httpClient   *http.Client
 	verbose      bool
-	CacheManager *cache.CacheManager
+	CacheManager cache.CacheManager
 	QueryIndex   map[string]string
 }
 
 var CurrentRepository Repository
 
-func NewRepository(cacheStrategy string, queryIndex map[string]string, verbose bool) error {
+func NewRepository(cacheManager cache.CacheManager, queryIndex map[string]string, verbose bool) error {
 	repo := Repository{
 		client:     config.CurrentSiteConfig.Client,
 		QueryIndex: queryIndex,
@@ -36,12 +36,7 @@ func NewRepository(cacheStrategy string, queryIndex map[string]string, verbose b
 	}
 	repo.httpClient = http.DefaultClient
 
-	cm, err := cache.NewCacheManager(cacheStrategy)
-	if err != nil {
-		return errors.New("Failed to initiate cache handler. " + " Error: " + err.Error())
-	}
-
-	repo.CacheManager = cm
+	repo.CacheManager = cacheManager
 
 	CurrentRepository = repo
 

commit 12851c72269c907b517d114ea25ee27061f72242

fix: bug causing --unused to allways report 0 items
fs.WalkDir() fails when given a path ending with "/", this wasn't
discovered earlier as the err variable was never checked.

Caused by: d63c1b98942e85ac2cc4da7ce74b66a0e54e02df

cmd/cache.go
@@ -65,12 +65,12 @@ var cacheCmd = &cobra.Command{
 
 			selectedCacheItems = append(selectedCacheItems, cacheLocation)
 		} else if len(args) == 0 && unusedOption {
-			usedItems, err := utils.ReadLineSeperatedFile(".snowman/last_build_queries.txt")
+			usedItems, err := utils.ReadLineSeperatedFile(snowmanPath + "/last_build_queries.txt")
 			if err != nil {
 				return utils.ErrorExit("Failed to read last unused cache items: ", err)
 			}
 
-			err = fs.WalkDir(os.DirFS("."), cacheLocation, func(path string, info fs.DirEntry, err error) error {
+			err = fs.WalkDir(os.DirFS("."), strings.TrimRight(cacheLocation, "/"), func(path string, info fs.DirEntry, err error) error {
 				if err != nil {
 					return err
 				}
@@ -88,6 +88,9 @@ var cacheCmd = &cobra.Command{
 				}
 				return nil
 			})
+			if err != nil {
+				return utils.ErrorExit("Failed to walk cache directory: ", err)
+			}
 
 			fmt.Println("Found " + fmt.Sprint(len(selectedCacheItems)) + " unused cache items.")
 		} else if len(args) == 1 {